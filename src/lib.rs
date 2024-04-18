ba1f25d387852cf5271221f6cbb336e29a839340
lapisprincess <lernertilda@gmail.com>
2024-04-17 21:40:49 -0700

    Final optimized version

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 use regex::Regex;
 
 pub fn scramble(words: Vec<&str>, mask: &str) -> Vec<String> {
-    let mut out: Vec<String> = Vec::new();
-    let expr = format!(r"\b({})\b", mask.replace("*", r"\w{1}"));
+    let expr = format!(r"\b({})\b", mask.replace("*", r"\w"));
     let re = Regex::new(&expr).unwrap();
-    for &word in words.iter().filter(|word| re.is_match(word)) {
-        out.push(String::from(word));
-    }
-    out
+    words.iter()
+        .filter(|word| re.is_match(word))
+        .map(|&word| String::from(word))
+        .collect()
 }
